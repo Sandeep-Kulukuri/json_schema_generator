1a84dd554aca8dddcd3efe00a2f6405d5b6ce0bf
Sandeep Kulukuri <52646165+Sandeep-Kulukuri@users.noreply.github.com>
2024-01-09 09:01:56 +0000

    Dynamic form initial version and changes has to be done. This is a stash commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,37 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [formData, setFormData] = useState([]);
+
+  const handleAddField = () => {
+    setFormData((prev) => [
+      ...prev,
+      { id: Date.now(), name: "", dataType: "", validationInstructions: "" },
+    ]);
+  };
+
   return (
     <>
       <h2>Dynamic Json Schema Generator</h2>
       <div className="form">
-        <div className="flex-container">
-          <label>Field Name</label>
-          <label> Data Type </label>
-        </div>
+        <form>
+          <div className="flex-container">
+            <label>Field Name</label>
+            <label>Data Type</label>
+            <button onClick={handleAddField}>Add Field</button>
+
+            {formData &&
+              formData.map((field) => (
+                
+                <ul key={field.id}>
+               <li>{field.name}</li>
+             </ul>
+              ))}
+          </div>
+        </form>
       </div>
       <div>
-        <h4> Json Schema Output </h4>
+        <h4>Json Schema Output</h4>
         <div className="schema-output">
           <button className="copy-btn">copy</button>
         </div>
